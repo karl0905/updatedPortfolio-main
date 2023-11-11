730bd5900309085f5f3f82e21d9acf0ea12ac73d
karl0905 <kalle44@live.dk>
2023-11-11 16:17:02 +0100

    new project added to projects

--- src/components/content/Projects.jsx
+++ src/components/content/Projects.jsx
@@ -2,7 +2,6 @@ import "./Projects.css";
 import Cardprojects from "../cardprojects/Cardprojects";
 
 export default function Projects() {
-
   return (
     <>
       <h2 className="section__title babas"> Projects</h2>
@@ -10,9 +9,30 @@ export default function Projects() {
         Her er et overblik over mine tidligere projekter
       </p>
       <div className="projects__container-flex inter">
-        <Cardprojects img="./webDoc.webp" description="En webdokumentar til kunstneren Mark Ebert" link="https://karlg.dk/webDoc-main/" github_link="https://github.com/karl0905/webDoc-main.git" alt="Et snapshot af hjemmesiden til kunstneren Mark Løvendahl"delay="300"/>
-        <Cardprojects img="./keramik.webp" description="Et websted til et keramikværksted i centrum af Aarhus" link="https://karlg.dk/keramik_2" github_link="https://github.com/karl0905/keramik_2.git" alt="Et snapshot af hjemmesiden til et keramikværksted" delay="400"/>
-        <Cardprojects img="./black.webp" description="More coming soon" hideButtons={true} alt="Et tomt sort billede"delay="500"/>
+        <Cardprojects
+          img="./bookbazr.webp"
+          description="En webapp til køb og salg af brugte studiebøger.
+          Udviklet i NextJS med Tailwind"
+          link="https://bookbazr.karlg.dk"
+          github_link="https://github.com/karl0905/webapp.git"
+          alt="Et snapshot af BookBazr siden"
+          delay="400"
+        />
+        <Cardprojects
+          img="./webDoc.webp"
+          description="En webdokumentar til kunstneren Mark Ebert"
+          link="https://karlg.dk/webDoc-main/"
+          github_link="https://github.com/karl0905/webDoc-main.git"
+          alt="Et snapshot af hjemmesiden til kunstneren Mark Løvendahl"
+          delay="300"
+        />
+        <Cardprojects
+          img="./black.webp"
+          description="More coming soon"
+          hideButtons={true}
+          alt="Et tomt sort billede"
+          delay="500"
+        />
       </div>
     </>
   );
